b5941cb65b61505311c73ceb8cd6294782c1d922
Karl George <karlgdev@gmail.com>
2022-04-21 12:40:20 +0200

    Making API call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
-}
+  const [jokes, setJokes] = useState();
 
+  useEffect(() => {
+    fetch(
+      'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart'
+    )
+      .then((res) => res.json())
+      .then((data) => setJokes(data));
+  }, []);
+
+  console.log(jokes);
+  return (
+    <div className='App'>
+      <h2>{jokes.setup}</h2>
+      <h2>{jokes.delivery}</h2>
+    </div>
+  );
+}
 export default App;
